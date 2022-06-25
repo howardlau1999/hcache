a4f291372ad5b229ca5bf548e9239f89f9a6cbc8
Howard Lau <howardlau1999@hotmail.com>
2022-06-25 16:21:59 +0800

    add git log to description

--- ros/pack-image.sh
+++ ros/pack-image.sh
@@ -4,7 +4,8 @@ source ./stop-ecs.sh
 echo "Wait for ECS to stop..."
 sleep 15
 export TS="$(date --rfc-3339='seconds' | tr ' +' '_')"
+export GIT_LOG="$(git log --format='%D %h %s' -n 1)"
 export UNIX_TS="$(date +%s)"
-aliyun ecs CreateImage --RegionId="cn-beijing" --Description="$TS" --InstanceId="$INSTANCE_ID" --ImageName="hcache-$TS" --Tag.1.Key="hcache" --Tag.1.Value="$UNIX_TS" | tee image.latest.json
+aliyun ecs CreateImage --RegionId="cn-beijing" --Description="$TS $GIT_LOG" --InstanceId="$INSTANCE_ID" --ImageName="hcache-$TS" --Tag.1.Key="hcache" --Tag.1.Value="$UNIX_TS" | tee image.latest.json
 export IMAGE_ID="$(jq -r '.ImageId' image.latest.json)"
 aliyun ecs ModifyImageSharePermission --ImageId="$IMAGE_ID" --RegionId="cn-beijing" --AddAccount.1="1828723137315221" || echo "Image is still creating, try again a few minutes later"
