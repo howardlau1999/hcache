1b908f3f5c9ca64eedfe25c0031d09dc2419b5fc
Howard Lau <howardlau1999@hotmail.com>
2022-06-27 00:27:11 +0800

    specify remote build branch

--- ros/remote-build.sh
+++ ros/remote-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xe
-./connect-ecs.sh "if [ -d hcache ]; then cd hcache && git pull; else git clone https://github.com/howardlau1999/hcache; fi"
+BRANCH=${1:-"master"}
+./connect-ecs.sh "if [ -d hcache ]; then cd hcache && git checkout $BRANCH && git pull --rebase; else git clone https://github.com/howardlau1999/hcache && git checkout $BRANCH; fi"
 ./connect-ecs.sh "rm -rf /data && mkdir -p /data"
 ./connect-ecs.sh "cd hcache && cargo build --release && cargo build"
 
