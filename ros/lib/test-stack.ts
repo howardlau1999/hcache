ca1fdff04c71abd3310bc4b53900101da68b3674
Howard Lau <howardlau1999@hotmail.com>
2022-06-26 20:06:51 +0800

    update ros scripts

--- ros/lib/test-stack.ts
+++ ros/lib/test-stack.ts
@@ -2,11 +2,62 @@ import * as ros from '@alicloud/ros-cdk-core';
 import * as ecs from '@alicloud/ros-cdk-ecs';
 import * as ROS from '@alicloud/ros-cdk-ros';
 
+const startupScriptFromCleanImage =  `#!/bin/bash
+
+      apt-get update && apt-get install -y build-essential curl git libclang-dev htop nfs-common tmux
+      curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain nightly
+      mkdir -p ~/.cargo
+      cat <<EOF > ~/.cargo/config
+[source.crates-io]
+replace-with = 'tuna'
+
+[source.tuna]
+registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
+EOF
+      cat <<EOF >> /etc/security/limits.conf 
+* hard memlock unlimited
+* soft memlock unlimited
+root hard nofile 1000000
+root soft nofile 1000000
+* hard nofile 1000000
+* soft nofile 1000000
+EOF
+      cat <<EOF >> /etc/sysctl.conf
+net.ipv4.ip_local_port_range = 1024 65535
+net.ipv4.ip_local_reserved_ports = 8080
+net.ipv4.tcp_fin_timeout = 15
+net.ipv4.tcp_tw_reuse = 1
+net.core.somaxconn = 32768
+net.ipv4.tcp_max_tw_buckets = 30000
+net.ipv4.tcp_sack = 1
+EOF
+      sysctl -p
+      NOTIFY
+      `;
+const imageAndStartScript = {
+  "debian": {
+    imageId: "debian_11_3_x64_20G_alibase_20220531.vhd",
+    startScript: startupScriptFromCleanImage,
+  },
+  "custom": {
+    imageId: "m-2ze6tbibqok06pny2wx3",
+    startScript: `#!/bin/bash
+    NOTIFY
+    `,
+  }
+}
+
 export class TestStack extends ros.Stack {
   constructor(scope: ros.Construct, id: string, props?: ros.StackProps) {
     super(scope, id, props);
     new ros.RosInfo(this, ros.RosInfo.description, "部署一个测试用的和用来打包镜像的按量付费 ECS");
     // The code that defines your stack goes here
+
+    // 指定使用的镜像和启动脚本
+    const fromWhich = "debian";
+    const spec = imageAndStartScript[fromWhich];
+    const specImageId = spec.imageId;
+    const specStartScript = spec.startScript;
 
     // 随机选择一个可用区部署
     const zoneId = ros.Fn.select(0, ros.Fn.getAzs(ros.RosPseudo.region));
@@ -30,9 +81,7 @@ export class TestStack extends ros.Stack {
     // 指定系统镜像、系统密码、实例类型
     const ecsImageId = new ros.RosParameter(this, "ecs_image_id", {
       type: ros.RosParameterType.STRING,
-      // defaultValue: "debian_11_3_x64_20G_alibase_20220531.vhd",
-      // 这个是编译过一次代码的镜像，基于这个开发会更快
-      defaultValue: "m-2ze6tbibqok06pny2wx3",
+      defaultValue: specImageId,
     });
     const ecsPassword = new ros.RosParameter(this, "ecs_password", {
       type: ros.RosParameterType.STRING,
@@ -78,45 +127,6 @@ export class TestStack extends ros.Stack {
       count: 1
     });
 
-    // 构建 ECS
-    // 如果是从 Debian 官方镜像创建的话，就需要安装工具链
-    const startupScriptFromCleanImage =  `#!/bin/bash
-
-      apt-get update && apt-get install -y build-essential curl git libclang-dev htop nfs-common
-      curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain nightly
-      mkdir -p ~/.cargo
-      cat <<EOF > ~/.cargo/config
-[source.crates-io]
-replace-with = 'tuna'
-
-[source.tuna]
-registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
-EOF
-      cat <<EOF >> /etc/security/limits.conf 
-* hard memlock unlimited
-* soft memlock unlimited
-root hard nofile 1000000
-root soft nofile 1000000
-* hard nofile 1000000
-* soft nofile 1000000
-EOF
-      cat <<EOF >> /etc/sysctl.conf
-net.ipv4.ip_local_port_range = 1024 65535
-net.ipv4.ip_local_reserved_ports = 8080
-net.ipv4.tcp_fin_timeout = 15
-net.ipv4.tcp_tw_reuse = 1
-net.core.somaxconn = 32768
-net.ipv4.tcp_max_tw_buckets = 30000
-net.ipv4.tcp_sack = 1
-EOF
-      sysctl -p
-      NOTIFY
-      `;
-    
-    // 用自己准备好的景象的话就不需要安装工具链
-    const startupScript = `#!/bin/bash
-    NOTIFY
-    `
     const ecsGroups = new ecs.InstanceGroup(this, 'hcache-test', {
       maxAmount: 1,
       vpcId: vpc.attrVpcId,
@@ -131,7 +141,7 @@ EOF
       allocatePublicIp: true,
       internetMaxBandwidthOut: 1,
       internetChargeType: 'PayByTraffic',
-      userData: ros.Fn.replace({ NOTIFY: ecsWaitConditionHandle.attrCurlCli }, startupScript),
+      userData: ros.Fn.replace({ NOTIFY: ecsWaitConditionHandle.attrCurlCli }, specStartScript),
     });
 
     new ros.RosOutput(this, 'instance_id', { value: ros.Fn.select(0, ecsGroups.getAtt('InstanceIds')) });
