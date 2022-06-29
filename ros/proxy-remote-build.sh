2bff17720e78b282211d25cd11e4877f361dc136
Howard Lau <howardlau1999@hotmail.com>
2022-06-30 00:26:52 +0800

    update module path

--- ros/proxy-remote-build.sh
+++ ros/proxy-remote-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 BRANCH=${1:-"master"}
+LOCAL_PROXY=${LOCAL_PROXY:-"192.168.237.1:10080"}
 echo -n "GitHub Username: "
 read USERNAME
 echo -n "GitHub Password: "
@@ -8,7 +9,7 @@ read PASSWORD
 URL="https://${USERNAME}:${PASSWORD}@ghproxy.com/https://github.com/howardlau1999/hcache"
 SOCKET_PATTERN="$HOME/.ssh/controlmasters/%r@%h:%p"
 mkdir -p ~/.ssh/controlmasters
-./connect-ecs.sh -M -S $SOCKET_PATTERN  -R "10080:192.168.237.1:10080" -N -f
+./connect-ecs.sh -M -S $SOCKET_PATTERN -R "10080:$LOCAL_PROXY" -N -f
 ./connect-ecs.sh "export HTTPS_PROXY=http://localhost:10080; if [ -d hcache ]; then \
 cd hcache && git fetch origin && git checkout $BRANCH && git pull --rebase $URL $BRANCH ;\
 else \
