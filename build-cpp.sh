cb0a0cfe430dbbf79619086db217d4b1772cd6c4
Howard Lau <howardlau1999@hotmail.com>
2022-07-01 16:07:10 +0800

    update build scripts

--- build-cpp.sh
+++ build-cpp.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -e
 CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-"RelWithDebInfo"}
 BUILD_DIR=cmake-build-$(echo $CMAKE_BUILD_TYPE | tr "[:upper:]" "[:lower:]")
-cmake -DRELWITHDEBINFO_FORCE_OPTIMIZATION_O3=ON -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -GNinja -B$BUILD_DIR .
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -GNinja -B$BUILD_DIR .
 cmake --build $BUILD_DIR
 cp $BUILD_DIR/cpp/hcache /usr/bin
--- build-rust.sh
+++ build-rust.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 pushd client && cargo build --release && popd
 pushd rust && cargo build --release && popd
 cp rust/target/release/hcache /usr/bin
